75fee62334d0ceaee61f05f58ceddeb7f43169d5
Insung0424 <zhrudrhks@gmail.com>
2023-01-18 18:56:10 +0900

    front 타입스크립트로 전환 완료

---
+++ Front/react_front/my_front_ts/src/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// ========================================
+class Screen extends React.Component {
+  render() {
+    return (
+      <div className='d1'>
+        <Welcome />
+        <div className='d2'>
+          <Btn />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Welcome extends React.Component {
+  render() {
+    return ( 
+      <div className='square'>
+        <h1>Hello, world</h1>
+        <p>폴더 경로 재작업 후 확인 완료</p>
+        <p>타입스크립트로 변경 후 확인 완료</p>
+      </div>
+    );
+  }
+}
+
+function Btn() {
+  type showButton = [show:boolean, setShow: boolean];
+
+  const showButton = React.useState(false)
+  const onClick = () => showButton[1]((prev) => !prev)
+
+  return (
+    <div className='two'>
+      <button className='btn' onClick={onClick}>
+        {showButton[0] ? 'OFF' : 'ON'}
+      </button>
+      {showButton[0] && <Img />}
+    </div>
+  )
+}
+
+const Img = ({Url, imgUrl, altDes}) => (
+    <div>
+      <br></br>
+      <a href={Url} >
+        <img src={imgUrl} alt={altDes}/>
+      </a>
+    </div>
+);
+
+Img.defaultProps = {
+  Url : "http://localhost:8080/",
+  imgUrl : '/images/Hello_World.png',
+  altDes : 'Hello_World.png'
+}
+
+ReactDOM.render(
+  <Screen />,
+  document.getElementById('root')
+);
